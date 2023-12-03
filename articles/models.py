1f0a5623001687f209dfe19cd7c95fadb0689509
MaximoRdz <mxrdhr@gmail.com>
2023-12-03 10:46:51 +0100

    Add  ImageField to database

--- articles/models.py
+++ articles/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     abstract = models.CharField(max_length=300, blank=True)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    thumbnail_image = models.ImageField(default="default.png", blank=True)
 
     def __str__(self):
         return self.title    
